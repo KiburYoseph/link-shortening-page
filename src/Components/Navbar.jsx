d9d0b61c6daa35f079d443be88d2362524fd63af
KiburYoseph <kiburstgr@gmail.com>
2023-09-03 10:28:29 +0300

    Hamburger Menu Functional

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,46 @@
+import { useState } from 'react'
 import React from 'react'
 import './Navbar.css'
 
+
+
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+
+
   return (
     <section className='navBar padded'>
         <img src='src/assets/logo.svg'></img>
-        <ul>
-            <li>Features</li>
-            <li>Pricing</li>
-            <li>Resources</li>
-        </ul>
-        <div className='navbarBtns'>
-            <button className='greyBtn'>Login</button>
-            <button>Sign Up</button>
-        </div>
+          <ul>
+              <li>Features</li>
+              <li>Pricing</li>
+              <li>Resources</li>
+          </ul>
+          
+          <div className='navbarBtns'>
+              <button className='greyBtn'>Login</button>
+              <button>Sign Up</button>
+          </div>
+          <button className='hamburgerBtn' onClick={() => setOpen(!open)}>
+            <img src='src/assets/burger-menu-svgrepo-com.svg'/>
+          </button>
+          { open ? 
+          
+          <div className='openHamburger'>
+                <ul>
+                  <li>Features</li>
+                  <li>Pricing</li>
+                  <li>Resources</li>
+                </ul>
+              <hr />
+              <button className='greyBtn login'>Login</button>
+              <button>Sign Up</button>
+          </div> 
+          
+          
+          
+          : null}
     </section>
   )
 }
